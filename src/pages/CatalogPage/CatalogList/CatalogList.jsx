f2d210c689430daf985140d6090905211ccc4381
Roman Popovych <popovychjob@gmail.com>
2025-07-18 12:20:51 +0300

    start CatalogList

---
+++ src/pages/CatalogPage/CatalogList/CatalogList.jsx
@@ -0,0 +1,10 @@
+"use client"
+import ProductCard from "@/components/ProductCard/ProductCard"
+import productService from "@/services/product.service"
+
+const CatalogList = () => {
+
+	
+}
+
+export default CatalogList
