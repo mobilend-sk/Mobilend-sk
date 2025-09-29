7d13a87130b35f954dd93647cd199dad04a6373f
Zlata <admin@mobilend.sk>
2025-09-29 09:58:22 +0200

    0.1

--- src/app/layout.js
+++ src/app/layout.js
@@ -117,12 +117,19 @@ export default function RootLayout({ children }) {
           `,
 					}}
 				/>
-				<script>{(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','GTM-MS49G67V')}
-				</script>
+				<Script id="gtm" strategy="afterInteractive">
+					{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MS49G67V');
+        `}
+				</Script>
 				<script
 					type="application/ld+json"
 					dangerouslySetInnerHTML={{
