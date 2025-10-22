cd263e0bf33eebfbf81cc256a757200d531e7061
Zlata basis <zl.maildesk@gmail.com>
2025-10-22 15:43:26 +0200

    2025_10_22_FixStyles

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -10,27 +10,27 @@ const ProductCard = ({ product }) => {
 	if (!product) return null
 
 	return (
-		<div 
-		className="ProductCard"
+		<div
+			className="ProductCard"
 		>
 			{
-						product.popular ? <div className="ProductCard__image-popular">
-							Populárne
-						</div> : ""
-					}
+				product.popular ? <div className="ProductCard__image-popular">
+					Populárne
+				</div> : ""
+			}
 			<Link href={`/katalog/${product.productLink}`}>
 				<div className="ProductCard__image">
 					<Image src={product.mainImage ? `${product.baseImageUrl}/${product.mainImage}` : product.images[0] ? `${product.baseImageUrl}/${product.images[0]}` : "/images/placeholder.webp"} alt="Image" width={200} height={220} />
-					{
-						product.discount ? <div className="ProductCard__image-discount">
-							-{product.discount}%
-						</div> : ""
-					}
 
-					
+
 				</div>
 				<h3>{product.model}</h3>
 			</Link>
+			{
+				product.discount ? <div className="ProductCard__image-discount">
+					-{product.discount}%
+				</div> : ""
+			}
 			<div className="ProductCard__bottom">
 				<div className="ProductCard__price">
 					<div className="ProductCard__price-total">
